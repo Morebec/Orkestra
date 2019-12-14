6b889963a5ec128ddc0f04f135c4d72cec7e4dcd
jwillp <jwilliam.perreault@morebec.com>
2019-12-14 11:04:46 +0100

    Project Generation - Fix DomainLayerConfiguration

--- src/ProjectGeneration/Domain/Model/Entity/Layer/Domain/DomainLayerConfiguration.php
+++ src/ProjectGeneration/Domain/Model/Entity/Layer/Domain/DomainLayerConfiguration.php
@@ -16,22 +16,22 @@ class DomainLayerConfiguration extends AbstractLayerConfiguration
 {
     public const LAYER_NAME = 'Domain';
 
-    public const ENTITIES_KEY = 'entities';
+    public const ENTITIES_KEY = 'Entity';
 
-    public const VALUE_OBJECTS_KEY = 'value_objects';
+    public const VALUE_OBJECTS_KEY = 'ValueObject';
 
-    public const COMMANDS_KEY = 'commands';
+    public const COMMANDS_KEY = 'Command';
 
-    public const EVENTS_KEY = 'events';
+    public const EVENTS_KEY = 'Event';
 
-    public const EXCEPTIONS_KEY = 'exceptions';
+    public const EXCEPTIONS_KEY = 'Exception';
 
-    public const SERVICES_KEY = 'services';
+    public const SERVICES_KEY = 'Service';
 
     public function __construct(?Description $description)
     {
         parent::__construct(self::LAYER_NAME,  [
-            'CommandObject',
+            'Command',
             'CommandHandler',
             'Exception',
             'Event',
@@ -50,22 +50,59 @@ class DomainLayerConfiguration extends AbstractLayerConfiguration
         if(array_key_exists(parent::DESCRIPTION_KEY, $data)) {
             $description = $data[parent::DESCRIPTION_KEY];
         }
-        $configuration = new static($description);
+        $layerConfiguration = new static($description);
 
+        $subDirNames = [];
+        $layerObjects = [];
+
+        foreach($data as $subDirName => $v) {
+            $subDirNames[] = $subDirName;
+            foreach($v as $object) {
+                $layerObjects[$subDirName][] = $object;
+            }
+        }
+
+        $specialKeys = [
+            self::ENTITIES_KEY,
+            self::VALUE_OBJECTS_KEY,
+            self::COMMANDS_KEY
+        ];
+
+        // Add the detected layerObjects to the layer
+        foreach ($layerObjects as $key => $keyObjects) {
+            if(in_array($key, $specialKeys)) continue;
+            foreach($keyObjects as $object) {
+                $objConfig = LayerObjectConfiguration::fromArray($moduleConfigurationFile, $object);
+                $layerConfiguration->addLayerObjectConfiguration($key, $objConfig);
+            }
+        }
+
+        // Handle Special Cases
         if(array_key_exists(self::ENTITIES_KEY, $data)) {
             $entities = $data[self::ENTITIES_KEY];
             foreach ($entities as $entity) {
                 $objConfig = LayerObjectConfiguration::fromArray($moduleConfigurationFile, $entity);
-                $configuration->addLayerObjectConfiguration(self::ENTITIES_KEY, $objConfig);
+                $layerConfiguration->addLayerObjectConfiguration(self::ENTITIES_KEY, $objConfig);
             }
         }
 
-/*        $valueObjects = $data[self::VALUE_OBJECTS_KEY];
-        $commands = $data[self::COMMANDS_KEY];
-        $events = $data[self::EVENTS_KEY];
-        $exceptions = $data[self::EXCEPTIONS_KEY];
-        $services = $data[self::SERVICES_KEY];*/
+        if(array_key_exists(self::VALUE_OBJECTS_KEY, $data)) {
+            $entities = $data[self::VALUE_OBJECTS_KEY];
+            foreach ($entities as $entity) {
+                $objConfig = LayerObjectConfiguration::fromArray($moduleConfigurationFile, $entity);
+                $layerConfiguration->addLayerObjectConfiguration(self::ENTITIES_KEY, $objConfig);
+            }
+        }
 
-        return $configuration;
+        if(array_key_exists(self::COMMANDS_KEY, $data)) {
+            $entities = $data[self::COMMANDS_KEY];
+            foreach ($entities as $entity) {
+                $objConfig = LayerObjectConfiguration::fromArray($moduleConfigurationFile, $entity);
+                $layerConfiguration->addLayerObjectConfiguration(self::COMMANDS_KEY, $objConfig);
+            }
+        }
+
+
+        return $layerConfiguration;
     }
 }
